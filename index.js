88f69ed3b4b344dce49df0bf6f57100f01b122e4
Déodat ADANDEDJAN <deodatadandedjan2000@gmail.com>
2024-03-18 18:19:16 +0100

    step5 | step6 : error_gestion | backendphonebook

--- index.js
+++ index.js
@@ -53,12 +53,44 @@ app.get("/api/persons/:id", (request, response) => {
   }
 });
 
-app.delete("/api/persons/:id", (request, response) =>{
-    const id = Number(request.params.id);
-    persons = persons.filter((person) => person.id !== id);
-    
-    response.status(204).end();
-})
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
+});
+
+const generateId = () => {
+  const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const existingPerson = persons.find((person) => person.name === body.name);
+  if (existingPerson) {
+    return response.status(409).json({
+      error: `${body.name} is already added. Name must be unique`,
+    });
+  }
+
+  const id = generateId();
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: id,
+  };
+
+  persons = persons.concat(person);
+  response.json(person);
+});
 
 const PORT = 3001;
 app.listen(PORT);
