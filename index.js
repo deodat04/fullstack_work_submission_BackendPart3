e1444435f2d0ba959c96d11335e947abe67034a2
Déodat ADANDEDJAN <deodatadandedjan2000@gmail.com>
2024-03-18 17:34:49 +0100

    step2 | route get for info

--- index.js
+++ index.js
@@ -30,9 +30,15 @@ app.get("/", (request, response) => {
     response.send("<h1>Hello World!</h1>");
 });
 
-app.get("/api/persons", (resquest, response) => {
+app.get("/api/persons", (request, response) => {
     response.json(persons);
 })
+
+app.get("/info", (request, response) => {
+    const date = new Date().toUTCString();
+    const numberOfPersons = persons.length;
+    response.send(`Phonebook has info for ${numberOfPersons} person(s)\n\nDate: ${date}`);
+});
 
 const PORT = 3001;
 app.listen(PORT);
